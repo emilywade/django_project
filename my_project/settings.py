2375e5fe757e91f0a58181f354005b1c94843573
Emily Wade <emilywade@Emilys-MacBook-Air.local>
2025-02-22 10:59:28 +0000

    testing debug

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^jcjv3$j%uhm&!t#8#fep0&cr*^hte5wu1*v+wbi@8kbzf9imf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
